1b1fb7d58459fe555c9a7abe58d50406f7e0d0af
Imran khan <141803706+Imranmpir@users.noreply.github.com>
2023-09-05 22:20:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import { Emplyee } from './componants/Employe';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Emplyee/>
+        <Emplyee/>
+        <Emplyee/>
+        <Emplyee/>
+        <Emplyee/>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis numquam cum adipisci quibusdam quaerat perspiciatis molestias esse neque officiis enim, alias assumenda explicabo facilis accusantium reprehenderit, eligendi dignissimos totam optio! Quod quidem voluptatibus perspiciatis iure fugiat. In totam ea rerum minima, delectus dolore iure porro expedita sit maxime deserunt autem nihil aliquid nam modi! Earum dolor inventore tenetur placeat sint qui saepe! Nulla quod neque commodi dolor, repellat natus tempore earum consequatur ipsam libero nemo, ut eligendi cum officia magni obcaecati. Incidunt recusandae delectus cum itaque, animi aperiam veritatis non doloremque minima laboriosam fugit facilis numquam quaerat deserunt ipsa doloribus, voluptates omnis quas dolor similique voluptatibus soluta vero autem! Quam vero ratione necessitatibus, odit quidem corrupti tenetur eius excepturi explicabo quas consequuntur aspernatur cumque, qui, eveniet pariatur esse! Ipsa perferendis assumenda accusamus iusto nihil quae, aut voluptatibus praesentium, architecto nam eaque sapiente obcaecati blanditiis quos ipsam? Qui similique ducimus iste rerum, alias quaerat necessitatibus officiis corrupti, magni molestiae, commodi reiciendis quam quasi quas nesciunt provident quod assumenda iure natus! Sit culpa odio dignissimos dolor minus quam sapiente temporibus voluptate excepturi asperiores veniam eum aspernatur architecto quasi ducimus amet illum reiciendis, quae harum. Sunt iusto optio vel, dicta sit aliquam unde nobis, quasi cum quaerat consequuntur perferendis nam facere quo? Recusandae perspiciatis, atque vel officia optio explicabo, reiciendis ipsum, quia beatae neque rerum illum quaerat unde quisquam natus consectetur. Similique accusamus eos inventore veniam, sunt accusantium cupiditate, vel sint amet ipsa quisquam eligendi officia? Voluptatum illum nobis tempore veritatis veniam quis eveniet dolor deleniti at. Tempore inventore architecto illum tempora aut doloribus quas natus laudantium est nam! Voluptates aut ratione est excepturi autem mollitia beatae facere totam dolor maxime. Ab maxime, a obcaecati perferendis sint sit rerum quasi at nostrum laborum magnam incidunt hic labore eligendi dicta consectetur officiis modi necessitatibus consequuntur enim laboriosam ducimus! Perferendis eligendi dignissimos accusamus fugiat cum? Obcaecati, recusandae nulla. Incidunt nobis dolorem repellat impedit delectus quia natus mollitia. Saepe, rem dolore. Aperiam quaerat dolorem laboriosam eius, amet iure rem maxime? Sapiente blanditiis omnis vero perspiciatis doloremque ab, voluptatibus animi officiis provident quam quas totam sunt dignissimos et doloribus nam, illo autem obcaecati, sit odit. Molestiae laudantium in praesentium voluptate id obcaecati neque quo est minus rerum corporis rem animi eius expedita sequi aut quisquam, voluptas molestias numquam ad quae asperiores libero maiores. Quos doloribus veniam placeat dolor nobis, provident id autem temporibus animi quis perferendis molestias possimus minima ad consequuntur quia exercitationem ullam, quisquam odio cumque neque. Ipsa quam explicabo dolorum, voluptates, laboriosam dignissimos tempore quisquam quos ipsum magni unde facere repellendus dolorem optio amet eum in nulla maiores est. Dolore enim id excepturi, laudantium tempora neque aperiam quod voluptatibus, animi eaque optio aspernatur alias? Recusandae optio dicta nulla nihil nam sit accusamus aut sed atque dignissimos? Laborum iure tempora architecto ab illo blanditiis voluptate explicabo delectus minima impedit veritatis laudantium, excepturi quos facere incidunt atque animi harum a cumque error! Mollitia aliquam, quidem illo amet facere quibusdam iste reiciendis ex architecto veniam error, necessitatibus repellendus.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componants/Employe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+export const Emplyee=()=>{
+return(
+    <div>
+        <h3>Node js is Express</h3>
+        <p>
+            lorem
+        </p>
+    </div>
+)
+}
